d796b03a947b36bc92df0ba6bd061915133014e5
James Kesten <jameskesten@Jamess-MacBook-Pro.local>
2014-04-01 20:08:36 +0000

    added a test class

---
+++ GitTest/JKMBFdog.h
@@ -0,0 +1,13 @@
+//
+//  JKMBFdog.h
+//  GitTest
+//
+//  Created by James Kesten on 2014-04-01.
+//  Copyright (c) 2014 James Kesten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKMBFdog : NSObject
+
+@end
---
+++ GitTest/JKMBFdog.m
@@ -0,0 +1,13 @@
+//
+//  JKMBFdog.m
+//  GitTest
+//
+//  Created by James Kesten on 2014-04-01.
+//  Copyright (c) 2014 James Kesten. All rights reserved.
+//
+
+#import "JKMBFdog.h"
+
+@implementation JKMBFdog
+
+@end
